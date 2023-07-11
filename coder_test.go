39eafd94d6d6b2be512c351e7a425594b0a174ed
Daniel Theophanes <kardianos@gmail.com>
2023-07-11 17:50:53 -0500

    lis2a: add e1394-97

--- coder_test.go
+++ coder_test.go
@@ -6,28 +6,41 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/kardianos/lis2a/e139497"
 	"github.com/kardianos/lis2a/lis2a2"
 )
 
 func TestRoundTrip(t *testing.T) {
-	fn := filepath.Join("testdata", "lis2a.txt")
-	ff, err := os.ReadFile(fn)
-	if err != nil {
-		t.Fatal(err)
+	list := []struct {
+		Filename string
+		Registry Registry
+	}{
+		{"lis2a.txt", lis2a2.Registry},
+		{"lis2a.txt", e139497.Registry},
+		{"esr.txt", e139497.Registry},
 	}
+	for _, item := range list {
+		t.Run(item.Filename, func(t *testing.T) {
+			fn := filepath.Join("testdata", item.Filename)
+			ff, err := os.ReadFile(fn)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	decode := NewDecoder(lis2a2.Registry, nil)
-	got, err := decode.Decode(ff)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("GOT: %+v", got)
+			decode := NewDecoder(item.Registry, nil)
+			got, err := decode.Decode(ff)
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Logf("GOT: %+v", got)
 
-	encode := NewEncoder(&EncodeOption{TrimTrailingSeparator: true})
-	gg, err := encode.Encode(got)
-	if err != nil {
-		t.Fatal(err)
+			encode := NewEncoder(&EncodeOption{TrimTrailingSeparator: true})
+			gg, err := encode.Encode(got)
+			if err != nil {
+				t.Fatal(err)
+			}
+			gg = bytes.ReplaceAll(gg, []byte{'\r'}, []byte{'\n'})
+			t.Logf("Round Trip:\n%s\n", gg)
+		})
 	}
-	gg = bytes.ReplaceAll(gg, []byte{'\r'}, []byte{'\n'})
-	t.Logf("Round Trip:\n%s\n", gg)
 }
